a9f4b29b01167e0f9028f2004b02813ab9c45f88
Gaetan ALLART <gaetan@nexylan.com>
2014-12-25 23:54:08 +0100

    detect shell script

--- scan.php
+++ scan.php
@@ -55,7 +55,7 @@ function detect_upload($filename) {
 }
 
 function detect_shell($filecontent) {
-	if (preg_match("/(\\x[0-9]{2})(?=\1{30})/",implode($filecontent))) {
+	if (preg_match("#/usr/bin/host#",implode($filecontent))) {
 		return true;
 	}
 	return false;
