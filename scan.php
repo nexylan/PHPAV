5c0b12d57a0da3139a732c2073620cdeef5258f1
Gaetan ALLART <gaetan@nexylan.com>
2014-12-27 03:31:48 +0100

    detect GLOBALS on 1st line

--- scan.php
+++ scan.php
@@ -41,7 +41,7 @@ function detect_obfuscated($filecontent) {
 
 // Detect eval functions on first line
 function detect_onelineshell($filecontent) {
-	if (isset($filecontent[0]) && preg_match("/eval\(/",$filecontent[0])) {
+	if (isset($filecontent[0]) && preg_match("/(GLOBALS|eval\()/",$filecontent[0])) {
 		return true;
 	}
 	return false;
