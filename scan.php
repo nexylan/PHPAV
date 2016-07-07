4250a0aa8ac026d44d00bec4606ca42b5f3ac8d2
Sullivan SENECHAL <soullivaneuh@gmail.com>
2016-07-07 14:10:51 +0200

    Apply CS
    Legend: ?-unknown, I-invalid file syntax, file ignored, S-Skipped, .-no changes, F-fixed, E-error
       1) scan.php (no_tab_indentation, header_comment, space_after_semicolon, single_quote, pre_increment, method_argument_space, no_alias_functions, binary_operator_spaces, concat_without_spaces, no_spaces_after_function_name, no_spaces_inside_parenthesis, no_closing_tag, blank_line_before_return, no_extra_consecutive_blank_lines, braces, single_blank_line_at_eof)

--- scan.php
+++ scan.php
@@ -1,6 +1,15 @@
 <?php
 
-ini_set('memory_limit','512M');
+/*
+ * This file is part of the Nexylan packages.
+ *
+ * (c) Nexylan SAS <contact@nexylan.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+ini_set('memory_limit', '512M');
 
 /*
     This file is part of PHPAV.
@@ -29,176 +38,175 @@ require_once __DIR__.'/vendor/autoload.php';
 // We are Gentoo users, we love color in bash shell :)
 $colors = new Colors();
 
-
 // Detect more than 10000 consecutive characters on first line
-function detect_obfuscated($filecontent) {
-	$weirdlength = 1000;
-	//if (isset($filecontent[1]) && strlen($filecontent[1]) > $weirdlength && preg_match("/[A-Za-z0-9\\\]{$weirdlength}/",$filecontent[1])) { // If a line contains more than 10,000 characters, write it to stdout
-	for ($line=0;$line<=1;$line++) {
-		if (isset($filecontent[$line]) && strlen($filecontent[$line]) > $weirdlength ) { // If a line contains more than 10,000 characters, write it to stdout
-			return true;
-		}
-	}
-	return false;
+function detect_obfuscated($filecontent)
+{
+    $weirdlength = 1000;
+    //if (isset($filecontent[1]) && strlen($filecontent[1]) > $weirdlength && preg_match("/[A-Za-z0-9\\\]{$weirdlength}/",$filecontent[1])) { // If a line contains more than 10,000 characters, write it to stdout
+    for ($line = 0; $line <= 1; ++$line) {
+        if (isset($filecontent[$line]) && strlen($filecontent[$line]) > $weirdlength) { // If a line contains more than 10,000 characters, write it to stdout
+            return true;
+        }
+    }
+
+    return false;
 }
 
 // Detect eval functions on first line
-function detect_onelineshell($filecontent) {
+function detect_onelineshell($filecontent)
+{
     $lines = 3;
-    for($i=0;$i<$lines;$i++) {
-    	if (isset($filecontent[$i]) && preg_match("/(eval[\s]*\(|system\(|\\\x)/",$filecontent[$i])) {
-    		return true;
-    	}
+    for ($i = 0; $i < $lines; ++$i) {
+        if (isset($filecontent[$i]) && preg_match("/(eval[\s]*\(|system\(|\\\x)/", $filecontent[$i])) {
+            return true;
+        }
     }
-    	if (isset($filecontent[sizeof($filecontent)-1]) && preg_match("/(eval\(|system\()/",$filecontent[sizeof($filecontent)-1])) {
-    		return true;
-	}
-	return false;
+    if (isset($filecontent[count($filecontent) - 1]) && preg_match("/(eval\(|system\()/", $filecontent[count($filecontent) - 1])) {
+        return true;
+    }
+
+    return false;
 }
 
 // Detect php files in upload folder
-function detect_upload($filename) {
-	if (preg_match("#/wp-content/uploads#",$filename) && filesize($filename) > 1024) {
-		return true;
-	}
-	return false;
+function detect_upload($filename)
+{
+    if (preg_match('#/wp-content/uploads#', $filename) && filesize($filename) > 1024) {
+        return true;
+    }
+
+    return false;
 }
 
 // Detect webshells patterns
-function detect_shell($filecontent) {
-	global $shells;
+function detect_shell($filecontent)
+{
+    global $shells;
 
-	foreach ($shells as $shell) {
-		if (strpos(implode($filecontent),trim($shell))) {
-			return true;
-		}
-	}
-	return false;
+    foreach ($shells as $shell) {
+        if (strpos(implode($filecontent), trim($shell))) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 // Display a report of infected file
-function report_file($file,$reason) {
-	global $colors;
+function report_file($file, $reason)
+{
+    global $colors;
 
-	echo $colors->getColoredString("Infected file (reason : $reason) :\n","red");
-    echo $colors->getColoredString("\t$file\n","light_blue");
+    echo $colors->getColoredString("Infected file (reason : $reason) :\n", 'red');
+    echo $colors->getColoredString("\t$file\n", 'light_blue');
 }
 
 // Delete the infected file with/without confirmation
-function delete_file($file,$content,$confirmation) {
+function delete_file($file, $content, $confirmation)
+{
     global $colors;
 
-    echo $colors->getColoredString("This file ($file) containing the following code :\n","cyan");
+    echo $colors->getColoredString("This file ($file) containing the following code :\n", 'cyan');
     echo "\t".$content."\n";
 
     if ($confirmation) {
-        echo $colors->getColoredString("Delete ? (y/n)","cyan");
-        $handle = fopen ("php://stdin","r");
+        echo $colors->getColoredString('Delete ? (y/n)', 'cyan');
+        $handle = fopen('php://stdin', 'r');
         $input = fgets($handle);
 
-        if (trim($input) == 'y')
-        {
+        if (trim($input) == 'y') {
             unlink($file);
-        }
-        else {
+        } else {
             echo "$input";
         }
-    }
-    else {
-        unlink ($file);
+    } else {
+        unlink($file);
     }
 }
 
 // Propose and apply patch
-function patch_file($file,$content) {
+function patch_file($file, $content)
+{
     global $colors;
 
-    $newfile = preg_replace("/^.*<\?php/","<?php",$content[0]);
+    $newfile = preg_replace("/^.*<\?php/", '<?php', $content[0]);
     $fp = fopen("$file.fixed", 'w');
-    fwrite ($fp,$newfile);
-    for ($i=1;$i<sizeof($content);$i++) {
-        fwrite($fp,$content[$i]);
+    fwrite($fp, $newfile);
+    for ($i = 1; $i < count($content); ++$i) {
+        fwrite($fp, $content[$i]);
     }
     fclose($fp);
     exec("diff -u $file $file.fixed > fix.patch");
 
-    if (filesize("fix.patch") > 0) {
-        $diff = file("fix.patch");
-        echo $colors->getColoredString( "I'm proposing the following patch. What do you think ?\n","cyan");
+    if (filesize('fix.patch') > 0) {
+        $diff = file('fix.patch');
+        echo $colors->getColoredString("I'm proposing the following patch. What do you think ?\n", 'cyan');
 
         echo "\n".implode($diff)."\n";
 
-
-        echo $colors->getColoredString("Apply ? (y/n)","cyan");
-        $handle = fopen ("php://stdin","r");
+        echo $colors->getColoredString('Apply ? (y/n)', 'cyan');
+        $handle = fopen('php://stdin', 'r');
         $input = fgets($handle);
         if (trim($input) == 'y') {
-                exec ("patch $file < fix.patch");
-		unlink ("fix.patch");
-		unlink ("$file.fixed");
+            exec("patch $file < fix.patch");
+            unlink('fix.patch');
+            unlink("$file.fixed");
+        } else {
+            echo 'No patch applied';
         }
-        else {
-            echo "No patch applied";
     }
-    }
-    unlink("fix.patch");
+    unlink('fix.patch');
 }
 
-
 // Main(void)
-if (empty($argv[1])) die("Usage: php find.php directory_to_scan > infected.txt\n");
-else {
-	echo $colors->getColoredString("Scanning " . $argv[1] . " for potential obfuscated malware...\n\n","green");
-	$data = array();
-        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($argv[1]),RecursiveIteratorIterator::SELF_FIRST); // Grab array of the entire structures of $argv[1] (a directory)
+if (empty($argv[1])) {
+    die("Usage: php find.php directory_to_scan > infected.txt\n");
+} else {
+    echo $colors->getColoredString('Scanning '.$argv[1]." for potential obfuscated malware...\n\n", 'green');
+    $data = array();
+    $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($argv[1]), RecursiveIteratorIterator::SELF_FIRST); // Grab array of the entire structures of $argv[1] (a directory)
         $c = 0; // Counter for files processed
         $f = 0; // Counter for files with potential malware
 
         // Preload data
         $shells = file(dirname(__FILE__).'/data/webshells.txt');
 
-        foreach ($files as $file)
-        {
-                //if (($c % 10000) == 0 && $c > 0) { // Display status for every 10,000 files
-                	//echo $colors->getColoredString("Processed " . $c . " files, found " . $f . "\n","purple");
+    foreach ($files as $file) {
+        //if (($c % 10000) == 0 && $c > 0) { // Display status for every 10,000 files
+                    //echo $colors->getColoredString("Processed " . $c . " files, found " . $f . "\n","purple");
                 //}
-                if (is_dir($file) === true) // Not in use, was used to check directory traversal was working properly
-                {
+                if (is_dir($file) === true) { // Not in use, was used to check directory traversal was working properly
                         //echo "Traversing into " . strval($file);
-                }
-                else { // If is file
-                        if (preg_match("/\.php$/",$file)) { // Currently only selects PHP scripts for scanning
+                } else { // If is file
+                        if (preg_match("/\.php$/", $file)) { // Currently only selects PHP scripts for scanning
                                 $arr = file($file); // Puts each line of the file into an array element
                                 if (detect_obfuscated($arr)) {
-                                    report_file($file,"obfuscated code on first line");
-                                    $f++;
-				    continue;
+                                    report_file($file, 'obfuscated code on first line');
+                                    ++$f;
+                                    continue;
                                 }
-                                if(detect_onelineshell($arr)) {
-                                    report_file($file,"First-line file with eval");
-                                    if (sizeof($arr) == 1) {
-                                        delete_file($file,implode($arr),true);
-                                    }
-                                    else {
-                                        patch_file($file,$arr);
-                                    }
-                                    $f++;
-				    continue;
+                            if (detect_onelineshell($arr)) {
+                                report_file($file, 'First-line file with eval');
+                                if (count($arr) == 1) {
+                                    delete_file($file, implode($arr), true);
+                                } else {
+                                    patch_file($file, $arr);
                                 }
-                                if (detect_upload ($file)) {
-                                    report_file($file,"PHP file in wordpress upload dir");
-                                    $f++;
-				    continue;
-                                }
-                                if (detect_shell($arr)) {
-                                    report_file($file,"Shell script pattern");
-                                    $f++;
-				    continue;
-                                }
+                                ++$f;
+                                continue;
+                            }
+                            if (detect_upload($file)) {
+                                report_file($file, 'PHP file in wordpress upload dir');
+                                ++$f;
+                                continue;
+                            }
+                            if (detect_shell($arr)) {
+                                report_file($file, 'Shell script pattern');
+                                ++$f;
+                                continue;
                             }
                         }
-                        $c++;
-                    }
                 }
-
-                ?>
+        ++$c;
+    }
+}
