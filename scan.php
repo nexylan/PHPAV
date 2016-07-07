9cd4d36f3f8ea7214674d007ff2635287720fced
Sullivan SENECHAL <soullivaneuh@gmail.com>
2016-07-07 14:11:14 +0200

    Remove old header comment

--- scan.php
+++ scan.php
@@ -11,29 +11,6 @@
 
 ini_set('memory_limit', '512M');
 
-/*
-    This file is part of PHPAV.
-
-    PHPAV is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    PHPAV is coded and maintained by Gaetan ALLART.
-
-    © Gaetan ALLART 2014
-
-    http://www.nexylan.com/
-*/
-
 require_once __DIR__.'/vendor/autoload.php';
 // We are Gentoo users, we love color in bash shell :)
 $colors = new Colors();
